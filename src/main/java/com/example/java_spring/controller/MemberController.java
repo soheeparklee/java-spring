0f294655ef95bb7c63637ecbe95cf09e7365b1e8
soheeparklee <soheeparklee@gmail.com>
2025-05-20 21:38:48 +0200

    test code transactional

--- src/main/java/com/example/java_spring/controller/MemberController.java
+++ src/main/java/com/example/java_spring/controller/MemberController.java
@@ -1,9 +1,15 @@
 package com.example.java_spring.controller;
 
+import com.example.java_spring.domain.Member;
 import com.example.java_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,25 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) { //dependency injection
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findAllMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.signUp(member);
+        return "redirect:/";
     }
 }
---
+++ src/test/java/com/example/java_spring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,58 @@
+package com.example.java_spring.service;
+
+import com.example.java_spring.domain.Member;
+import com.example.java_spring.repository.MemberRepository;
+import com.example.java_spring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+//@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository repository;
+    @Test
+    void signUp() {
+        //given
+        Member member = new Member();
+        member.setName("aaa");
+        //when
+        Long saveId = memberService.signUp(member);
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(findMember.getName()).isEqualTo(member.getName());
+    }
+
+    @Test
+    void signUpDuplicate() {
+        //given
+        Member member1 = new Member();
+        member1.setName("bbb");
+
+        Member member2 = new Member();
+        member2.setName("bbb"); //same name trying to signUp
+        //when
+        memberService.signUp(member1);
+        //then
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.signUp(member2));
+        assertThat(e.getMessage()).isEqualTo("member already exists");
+
+
+        /*try{
+            memberService.signUp(member2);
+            fail();
+        }catch(IllegalArgumentException e){
+            assertThat(e.getMessage()).isEqualTo("member already exists");
+        }*/
+
+
+    }
+
+}
