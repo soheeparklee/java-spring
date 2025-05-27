4968c832630e6a542e490603c80e39117b7ada26
soheeparklee <soheeparklee@gmail.com>
2025-05-27 19:59:26 +0200

    recommend constructor DI

---
+++ src/test/java/com/example/spring_basic/autowired/OrderServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.example.spring_basic.autowired;
+
+import com.example.spring_basic.discount.RateDiscountPolicy;
+import com.example.spring_basic.member.MemoryMemberRepository;
+import com.example.spring_basic.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder(){
+        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        orderService.createOrder(1L, "mem", 1000);
+    }
+
+}
