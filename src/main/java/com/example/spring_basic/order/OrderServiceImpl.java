2cad52e887c1ef8e9f607e564054f0080220601f
soheeparklee <soheeparklee@gmail.com>
2025-05-27 23:20:47 +0200

    lombok

--- src/main/java/com/example/spring_basic/order/OrderServiceImpl.java
+++ src/main/java/com/example/spring_basic/order/OrderServiceImpl.java
@@ -6,29 +6,15 @@ import com.example.spring_basic.discount.FixDiscountPolicy;
 import com.example.spring_basic.discount.RateDiscountPolicy;
 import com.example.spring_basic.member.*;
 import com.example.spring_basic.order.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
-
-//    @Autowired private  MemberRepository repository;
-//    @Autowired private  DiscountPolicy discountPolicy;
-
-//    private final MemberRepository repository = new MemoryMemberRepository();
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-
     private final MemberRepository repository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository repository, DiscountPolicy discountPolicy) {
-        this.repository = repository;
-        this.discountPolicy = discountPolicy;
-    }
-
-
 
     @Override
     public Order createOrder(Long memberId, String name, int price) {
